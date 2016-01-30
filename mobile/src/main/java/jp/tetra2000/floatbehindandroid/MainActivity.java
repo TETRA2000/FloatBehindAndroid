8cb3964459fa591fc57ba02e2735f553edff08a5
TETRA2000 <sagittariusm25@gmail.com>
2016-01-30 15:04:55 +0900

    install crosswalk

--- mobile/src/main/java/jp/tetra2000/floatbehindandroid/MainActivity.java
+++ mobile/src/main/java/jp/tetra2000/floatbehindandroid/MainActivity.java
@@ -3,11 +3,17 @@ package jp.tetra2000.floatbehindandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.xwalk.core.XWalkView;
+
 public class MainActivity extends AppCompatActivity {
+    private XWalkView xWalkWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        xWalkWebView=(XWalkView)findViewById(R.id.xwalkWebView);
+        xWalkWebView.load("https://crosswalk-project.org", null);
     }
 }
