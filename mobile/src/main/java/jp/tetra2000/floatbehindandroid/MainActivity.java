c18ebec8df950cf9171ae6d7ef9176fb4710d6cd
TETRA2000 <sagittariusm25@gmail.com>
2016-01-30 16:47:13 +0900

    change to webview

--- mobile/src/main/java/jp/tetra2000/floatbehindandroid/MainActivity.java
+++ mobile/src/main/java/jp/tetra2000/floatbehindandroid/MainActivity.java
@@ -1,19 +1,40 @@
 package jp.tetra2000.floatbehindandroid;
 
+import android.app.WallpaperManager;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import org.xwalk.core.XWalkView;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
-    private XWalkView xWalkWebView;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        xWalkWebView=(XWalkView)findViewById(R.id.xwalkWebView);
-        xWalkWebView.load("https://crosswalk-project.org", null);
+        final WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
+        final Drawable wallpaperDrawable = wallpaperManager.getDrawable();
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        relativeLayout.setBackground(wallpaperDrawable);
+
+        webView =(WebView)findViewById(R.id.xwalkWebView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.setBackgroundColor(Color.TRANSPARENT);
+        webView.loadUrl("http://floatbehind.japaneast.cloudapp.azure.com:3000/oauth/slack", null);
     }
 }
