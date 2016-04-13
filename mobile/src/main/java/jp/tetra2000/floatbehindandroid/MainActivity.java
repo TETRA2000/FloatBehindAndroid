a642dee81428b3503c7ef7a763665c4d836f5763
TETRA2000 <sagittariusm25@gmail.com>
2016-04-13 23:27:22 +0900

    set xwalkview bg transparent

--- mobile/src/main/java/jp/tetra2000/floatbehindandroid/MainActivity.java
+++ mobile/src/main/java/jp/tetra2000/floatbehindandroid/MainActivity.java
@@ -1,8 +1,13 @@
 package jp.tetra2000.floatbehindandroid;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
 
+import org.xwalk.core.XWalkPreferences;
 import org.xwalk.core.XWalkView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +18,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        xWalkWebView=(XWalkView)findViewById(R.id.xwalkWebView);
+        XWalkPreferences.setValue(XWalkPreferences.ANIMATABLE_XWALK_VIEW, true);
+
+        ViewGroup rootLayout = (ViewGroup) findViewById(R.id.container);
+        xWalkWebView = new XWalkView(this);
+        xWalkWebView.setBackgroundColor(Color.TRANSPARENT);
         xWalkWebView.load("https://crosswalk-project.org", null);
+        rootLayout.addView(xWalkWebView);
+
+        xWalkWebView.setBackgroundColor(Color.TRANSPARENT);
+        TextureView tuv = findXWalkTextureView(xWalkWebView);
+        tuv.setOpaque(false);
+    }
+
+    private TextureView findXWalkTextureView(ViewGroup group) {
+        int childCount = group.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = group.getChildAt(i);
+            if (child instanceof TextureView) {
+                String parentClassName = child.getParent().getClass().toString();
+                boolean isRightKindOfParent = (parentClassName.contains("XWalk"));
+                if (isRightKindOfParent)
+                { return (TextureView) child; }
+            } else if (child instanceof ViewGroup) {
+                TextureView textureView = findXWalkTextureView((ViewGroup) child);
+                if (textureView != null)
+                { return textureView; }
+            }
+        }
+        return null;
     }
 }
